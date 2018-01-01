6c8209a125d83122e2cdbc84e16de5ebfd502248
Robert Clayton <robbie.c.clayton@gmail.com>
2018-01-01 15:13:02 +0000

    added sidebar
    responsively disappears when view is small and  adjusts the top banner view through the phone when the banner disappears

--- jquery.js
+++ jquery.js
@@ -1,5 +1,13 @@
 $(document).mousemove(function(mouse){
-  var xPostition = mouse.pageX
+  var query = $('#sidebar');
+  var isVisible = query.is(':visible');
+  if (isVisible === true) {
+    console.log("sidebar active")
+    var xPostition = (mouse.pageX - 175)
+  } else {
+    var xPostition = mouse.pageX
+    console.log("sidebar not visible")
+  }
   var yPostition = mouse.pageY
 
   $("#phone").css({left:xPostition , top:yPostition});
@@ -12,17 +20,23 @@ $(document).mousemove(function(mouse){
   topParam = yPostition - (phoneHeight / 3.4)
   bottomParam = backgroundHeight - yPostition - (phoneHeight / 3.5)
   leftParam = xPostition - (phoneWidth / 2.1)
+  // leftParam = xPostition - (phoneWidth / 1.03)
   rightParam = backgroundWidth - xPostition - (phoneWidth / 2.2)
+  // rightParam = backgroundWidth - xPostition - (phoneWidth / 6)
 
-  console.log(backgroundHeight)
+  console.log(backgroundWidth)
 
   $("#colour-background").css('-webkit-clip-path', 'inset('+topParam+'px '+rightParam+'px '+bottomParam+'px '+leftParam+'px)');
 });
 
 $(document).ready(function () {
+  // $('#sidebarCollapse').on('click', function () {
+  //     $('#sidebar').toggleClass('active');
+  // });
 
-    $('#sidebarCollapse').on('click', function () {
-        $('#sidebar').toggleClass('active');
-    });
+  // $("#sidebar").mCustomScrollbar({
+  //   theme: "minimal"
+  // });
+
 
 });
